6c35b941e402b40588271edf2a5e212f37fcf743
Adrián Hernández <adrian.elca15@gmail.com>
2022-06-07 12:07:11 -0400

    modificanto el button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({action, isActive}) => {
   return (
-    <button className='btn-card' onClick={action}>Change {isActive?'degrees °F':'degrees ℃'}</button>
+    <button className='btn-card' onClick={action}>Change {isActive?' fahrenheit °F':' celsius ℃'}</button>
   )
 }
 
